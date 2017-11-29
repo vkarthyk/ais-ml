6ffe4c2612caff14be9882fa6eff83f9c225cbda
xhyumiracle <xhyumiracle@gmail.com>
2017-11-29 13:48:21 -0500

    modified main_nn.py, sent to zuo

--- ml/indicatorStructure/main_nn.py
+++ ml/indicatorStructure/main_nn.py
@@ -8,24 +8,25 @@ MODEL_SAVE_PATH = 'CNN1d_all_10000st_1e-3lr.target20.mdl'
 # MODEL_SAVE_PATH = 'CNN1d_all_1000st_1e-2lr.mdl'
 nnleanring = IndicatorStructureNN(max_row_per_time=10000)
 nnleanring.set_learning_class(CNND1ClassifierLearning, target_num=20)
-nnleanring.train(lr=1e-1, steps=100
-                 , featurepath=FEATURE_PATH, targetpath=TARGET_PATH, targetcol=TARGET_COL
-                 , featurecolblacklist=['stix_name']
-                 # , rowlist=range(80)
-                 )
-nnleanring.save('1'+MODEL_SAVE_PATH)
-nnleanring.train(lr=1e-2, steps=100
-                 , featurepath=FEATURE_PATH, targetpath=TARGET_PATH, targetcol=TARGET_COL
-                 , featurecolblacklist=['stix_name']
-                 # , rowlist=range(80)
-                 )
-nnleanring.save('2'+MODEL_SAVE_PATH)
-nnleanring.train(lr=1e-3, steps=1000
-                 , featurepath=FEATURE_PATH, targetpath=TARGET_PATH, targetcol=TARGET_COL
-                 , featurecolblacklist=['stix_name']
-                 # , rowlist=range(80)
-                 )
-nnleanring.save('3'+MODEL_SAVE_PATH)
+# nnleanring.train(lr=1e-1, steps=100
+#                  , featurepath=FEATURE_PATH, targetpath=TARGET_PATH, targetcol=TARGET_COL
+#                  , featurecolblacklist=['stix_name']
+#                  # , rowlist=range(80)
+#                  )
+# nnleanring.save('1'+MODEL_SAVE_PATH)
+# nnleanring.train(lr=1e-2, steps=100
+#                  , featurepath=FEATURE_PATH, targetpath=TARGET_PATH, targetcol=TARGET_COL
+#                  , featurecolblacklist=['stix_name']
+#                  # , rowlist=range(80)
+#                  )
+# nnleanring.save('2'+MODEL_SAVE_PATH)
+# nnleanring.train(lr=1e-3, steps=1000
+#                  , featurepath=FEATURE_PATH, targetpath=TARGET_PATH, targetcol=TARGET_COL
+#                  , featurecolblacklist=['stix_name']
+#                  # , rowlist=range(80)
+#                  )
+# nnleanring.save('3'+MODEL_SAVE_PATH)
+nnleanring.load('3'+MODEL_SAVE_PATH, D_in=270, target_num=20)
 nnleanring.train(lr=1e-4, steps=1000
                  , featurepath=FEATURE_PATH, targetpath=TARGET_PATH, targetcol=TARGET_COL
                  , featurecolblacklist=['stix_name']
